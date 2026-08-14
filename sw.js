8b9ab92ebcfb1bd0a441e2b24209903bf2877a72
inforicbm-ric <inforic.bm@gmail.com>
2026-08-14 11:03:34 -0300

    novo visual

--- sw.js
+++ sw.js
@@ -1,5 +1,38 @@
-const CACHE_NAME = 'orcafacil-v6';
-const urlsToCache = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))); self.skipWaiting(); });
-self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then(n => Promise.all(n.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))))); self.clients.claim(); });
-self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => e.request.mode === 'navigate' ? caches.match('./index.html') : undefined))); });
+const CACHE_NAME = 'orcafacil-v7';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-72.png',
+  './icons/icon-96.png',
+  './icons/icon-128.png',
+  './icons/icon-144.png',
+  './icons/icon-152.png',
+  './icons/icon-192.png',
+  './icons/icon-384.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(names => Promise.all(
+    names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
+  )));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => {
+      if (r) return r;
+      return fetch(e.request).catch(() => {
+        if (e.request.mode === 'navigate') return caches.match('./index.html');
+        return undefined;
+      });
+    })
+  );
+});
